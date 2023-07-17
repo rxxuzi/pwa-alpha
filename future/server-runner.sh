fc51e3c0660ff0670d337b6c7602087a93d18cfe
rxxuzi <75666622+rxxuzi@users.noreply.github.com>
2023-07-17 18:51:02 +0900

    add server-runner.sh

---
+++ future/server-runner.sh
@@ -0,0 +1,45 @@
+# cd alpha directory
+# shellcheck disable=SC2034
+for i in $(seq 1 10); do
+  ls -a
+    if [ -d alpha ]; then
+        # shellcheck disable=SC2164
+        cd alpha
+        echo "alpha directory exists" >> now.txt
+        break
+    fi
+    cd ..
+done
+
+echo "alpha directory exists"
+
+if [ ! -d C:/Apache/Apache24/htdocs/alpha ]; then
+    mkdir C:/Apache/Apache24/htdocs/alpha
+fi
+
+cp -r ./* C:/Apache/Apache24/htdocs/alpha/
+
+# shellcheck disable=SC2164
+cd C:/Apache/Apache24/htdocs/alpha/
+
+echo "alpha copied"
+echo "Run Apache Server ? "
+
+yn="no"
+
+while true; do
+    read -p "y/n?" yn
+    case $yn in
+        [Yy]* ) yn="yes"; break;;
+        [Nn]* ) yn="no"; break;;
+        * ) echo "Please answer yes or no.";;
+    esac
+done
+
+# then run the server
+if [ $yn = "yes" ]; then
+    echo "Apache Server Running"
+    # shellcheck disable=SC2164
+    cd C:/Apache/Apache24/bin/
+    ./httpd.exe
+fi
